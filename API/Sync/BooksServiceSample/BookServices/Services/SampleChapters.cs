3edc09ed90772d5d435db0e2e42ef556c3b3b69b
Bob Zhang <bobzhang55@yahoo.com>
2025-03-31 16:56:38 -0500

    minor change

--- API/Sync/BooksServiceSample/BookServices/Services/SampleChapters.cs
+++ API/Sync/BooksServiceSample/BookServices/Services/SampleChapters.cs
@@ -5,6 +5,24 @@ namespace BooksServiceSample.Services
 {
     public class SampleChapters
     {
+        private readonly string[] _sampleTitles = new[]
+        {
+            ".NET Application Architectures",
+            "Core C#",
+            "Objects and Types",
+            "Object-Oriented Programming with C#",
+            "Generics",
+            "Operators and Casts",
+            "Arrays",
+            "Delegates, Lambdas, and Events",
+            "Windows Communication Foundation"
+        };
+        private readonly int[] _chapterNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 44 };
+        private readonly int[] _numberPages = { 35, 42, 33, 20, 24, 38, 20, 32, 44 };
+
+        /// <summary>
+        /// 
+        /// </summary>
         private readonly IBookChaptersService _bookChaptersService;
         public SampleChapters(IBookChaptersService bookChapterService)
         {
@@ -25,20 +43,5 @@ namespace BooksServiceSample.Services
             }
             _bookChaptersService.AddRange(chapters);
         }
-
-        private readonly string[] _sampleTitles = new[]
-        {
-            ".NET Application Architectures",
-            "Core C#",
-            "Objects and Types",
-            "Object-Oriented Programming with C#",
-            "Generics",
-            "Operators and Casts",
-            "Arrays",
-            "Delegates, Lambdas, and Events",
-            "Windows Communication Foundation"
-        };
-        private readonly int[] _chapterNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 44 };
-        private readonly int[] _numberPages = { 35, 42, 33, 20, 24, 38, 20, 32, 44 };
     }
 }
